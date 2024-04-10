edecec14ab6583257fd04f1d709a18d226709843
DavidBuenoC <davidbuenocarmona@gmail.com>
2024-04-10 15:57:27 +0200

    Creado proyecto FitPro

---
+++ FitPro/src/main/java/uma/fitpro/FitProApplication.java
@@ -0,0 +1,13 @@
+package uma.fitpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitProApplication.class, args);
+	}
+
+}
---
+++ FitPro/src/test/java/uma/fitpro/FitProApplicationTests.java
@@ -0,0 +1,13 @@
+package uma.fitpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

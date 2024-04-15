cb925bf5e33bcf374f3cf014103d61fd901ba0c8
DavidBuenoC <davidbuenocarmona@gmail.com>
2024-04-15 11:47:29 +0200

    Created controller

--- FitPro/src/main/java/uma/fitpro/controller/LoginController.java
+++ FitPro/src/main/java/uma/fitpro/controller/LoginController.java
@@ -1,9 +1,11 @@
 package uma.fitpro.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class LoginController {
     @GetMapping("/")
     public String doLogin() {

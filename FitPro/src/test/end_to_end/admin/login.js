3d74835418ebc29f661f9d1f9a0b18a85a905d85
DavidBuenoC <davidbuenocarmona@gmail.com>
2024-05-29 11:08:48 +0200

    Tests de Admin

---
+++ FitPro/src/test/end_to_end/admin/login.js
@@ -0,0 +1,43 @@
+import { browser } from 'k6/experimental/browser';
+import { check, sleep } from 'k6';
+
+export const options = {
+  scenarios: {
+    ui: {
+      executor: 'shared-iterations',
+      options: {
+        browser: {
+          type: 'chromium',
+        },
+      },
+    } 
+  },
+  thresholds: {
+    checks: ["rate==1.0"]
+  }
+}
+
+export default async function () {
+  const page = browser.newPage();
+  try {
+    await page.goto('http://localhost:8080/');
+
+    page.locator('input[name="mail"]').clear();
+    page.locator('input[name="password"]').clear();
+
+    page.locator('input[name="mail"]').type('david@gmail.com');
+    page.locator('input[name="password"]').type('david');
+
+    const loginButton = page.locator('button[name="login"]');
+    sleep(3);
+    await Promise.all([page.waitForNavigation({waitUntil: 'networkidle'}), loginButton.click()]);
+
+    check(page, {
+      'Title': p => p.locator('h1').textContent() == 'Admin Home',
+    });
+    sleep(3);
+
+  } finally {
+    page.close();
+  }
+}
